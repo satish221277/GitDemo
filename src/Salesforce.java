c6e6cd8f6b16d1e038388717c3136e5f7e4eb1f5
Satish <satish.kumar@rategain.com>
2021-06-21 17:20:31 +0530

    changes in salesforce

--- src/Salesforce.java
+++ src/Salesforce.java
@@ -10,10 +10,11 @@ public class Salesforce {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\satish.kumar\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("http://bg.rategain.com/login#internal");
-		//driver.findElement(By.xpath("//*[@id=\'input_0\']")).sendKeys("satish.k2010@yahoo.com");
+		driver.findElement(By.xpath("//*[@id=\'input_0\']")).sendKeys("satish.k2010@yahoo.com");
 		driver.findElement(By.cssSelector("input[name*='user']")).sendKeys("satish.k2010@yahoo.com");
 		driver.findElement(By.xpath("//*[@id=\'input_1\']")).sendKeys("123");
 		driver.findElement(By.xpath("/html/body/ui-view/div/md-content/div[1]/form/md-content/div/button")).click();
+		// This is just a comment 
 	}
 
 }
